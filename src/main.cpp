bf9ed5097250d74b3d7d0f80a1c2af9b2cf860ab
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-23 00:08:51 -0300

    feat: add initial code to read sensor values, add HTU21DF lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
-#include <Arduino.h>
+#include "Adafruit_HTU21DF.h"
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
+
+#define LDR_PIN 34
+#define RAIN_SENSOR_PIN 35
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  if (!htu.begin()) {
+    Serial.println("Couldn't find HTU21D, check wiring!");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.println("--------");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // HTU21D sensor (humidity and temperature)
+  float humidity = htu.readHumidity();
+  float temp_h = htu.readTemperature();
+  Serial.print(F("Humidity (HTU21D): "));
+  Serial.print(humidity);
+  Serial.println(F("%"));
+  Serial.print(F("Temperature (HTU21D): "));
+  Serial.print(temp_h);
+  Serial.println(F("°C"));
+
+  // Light sensor
+  int lightLevel = analogRead(LDR_PIN);
+  Serial.print(F("Light Level: "));
+  Serial.println(lightLevel);
+
+  // Rain sensor
+  int rainLevel = analogRead(RAIN_SENSOR_PIN);
+  Serial.print(F("Rain Level: "));
+  Serial.println(rainLevel);
+
+  Serial.println("--------");
+
+  delay(5000);
 }
